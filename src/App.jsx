af7d3eed009cd32951b9d4af9d7622bca5c8d746
GE Bharath <bharathmdks7@gmail.com>
2026-06-17 10:59:09 +0530

    Initial React App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome Bharath</h1>
+      <h2>Vision Solve AI</h2>
+      <p>My First React Application deployed using AWS Amplify</p>
+    </div>
+  );
+}
+
+export default App;
